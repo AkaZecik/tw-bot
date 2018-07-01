e1223c52610e77a3afd64dd9e005067586c522de
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2018-07-01 19:36:32 +0200

    Add option to choose farming group

--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
+chrome.runtime.onMessageExternal.addListener(function (message, sender, sendResponse) {
+    if (message) {
+        if (message.name === "getGameData") {
+            let farming_group = $("#farming_group");
+            let groups = message.groups.result;
+
+            for (let i = 0; i < groups.length; ++i) {
+                let groupID = groups[i].group_id;
+                let groupName = groups[i].name;
+                farming_group.append(
+                    $("<option value=\"" + groupID + "\">" + groupName + "</option>")
+                );
+            }
+        }
+    }
+
+    return true;
+});
+
 $(function () {
     chrome.runtime.sendMessage({name: "getSettings"}, function (settings) {
         $("#farming_label").val(settings["TW-Bot/farming_label"]);
@@ -19,5 +38,18 @@ $(function () {
 
     $("#go_to_options").click(function () {
         chrome.runtime.openOptionsPage();
+    });
+
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        injectFunction(tabs[0].id, function () {
+            TribalWars.get("groups", {ajax: "load_groups"}, function (groups) {
+                chrome.runtime.sendMessage(TW_Bot_id, {
+                    name: "getGameData",
+                    groups: groups,
+                    hostname: "https://" + window.location.hostname,
+                    game_data: game_data
+                });
+            });
+        });
     });
 });
