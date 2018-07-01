05ce8fcf50f9c94a134b9316cd638be958ca5384
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2018-07-01 19:54:30 +0200

    Show message when at the wrong page

--- popup.js
+++ popup.js
@@ -21,7 +21,35 @@ chrome.runtime.onMessageExternal.addListener(function (message, sender, sendResp
 });
 
 $(function () {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.query({active: true, currentWindow: true, url: "https://*.plemiona.pl/*"}, function (tabs) {
+        if (tabs.length === 0) {
+            $("#loading").hide();
+            $("#wrong_page").show();
+            return;
+        }
+
+        chrome.runtime.sendMessage({name: "getSettings"}, function (settings) {
+            $("#farming_label").val(settings["TW-Bot/farming_label"]);
+            $("#wall_max").val(settings["TW-Bot/wall_max"]);
+            $("#distance_max").val(settings["TW-Bot/distance_max"]);
+            $("#click_time_min").val(settings["TW-Bot/click_time_min"]);
+            $("#click_time_max").val(settings["TW-Bot/click_time_max"]);
+            $("#farming_sleep_min").val(settings["TW-Bot/farming_sleep_min"]);
+            $("#farming_sleep_max").val(settings["TW-Bot/farming_sleep_max"]);
+            $("#spy_required").prop("checked", settings["TW-Bot/spy_required"]);
+            $("#farming_order").val(settings["TW-Bot/farming_order"]);
+        });
+
+        let wall_max = $("#wall_max");
+
+        for (i = 0; i <= 20; ++i) {
+            wall_max.append($("<option value=\"" + i + "\">" + i + "</option>"));
+        }
+
+        $("#go_to_options").click(function () {
+            chrome.runtime.openOptionsPage();
+        });
+
         injectFunction(tabs[0].id, function () {
             TribalWars.get("groups", {ajax: "load_groups"}, function (groups) {
                 chrome.runtime.sendMessage(TW_Bot_id, {
@@ -32,27 +60,5 @@ $(function () {
                 });
             });
         });
-    });
-
-    chrome.runtime.sendMessage({name: "getSettings"}, function (settings) {
-        $("#farming_label").val(settings["TW-Bot/farming_label"]);
-        $("#wall_max").val(settings["TW-Bot/wall_max"]);
-        $("#distance_max").val(settings["TW-Bot/distance_max"]);
-        $("#click_time_min").val(settings["TW-Bot/click_time_min"]);
-        $("#click_time_max").val(settings["TW-Bot/click_time_max"]);
-        $("#farming_sleep_min").val(settings["TW-Bot/farming_sleep_min"]);
-        $("#farming_sleep_max").val(settings["TW-Bot/farming_sleep_max"]);
-        $("#spy_required").prop("checked", settings["TW-Bot/spy_required"]);
-        $("#farming_order").val(settings["TW-Bot/farming_order"]);
-    });
-
-    let wall_max = $("#wall_max");
-
-    for (i = 0; i <= 20; ++i) {
-        wall_max.append($("<option value=\"" + i + "\">" + i + "</option>"));
-    }
-
-    $("#go_to_options").click(function () {
-        chrome.runtime.openOptionsPage();
     });
 });
